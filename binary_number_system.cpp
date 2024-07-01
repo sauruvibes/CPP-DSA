7527d300cd531e380d700a0aab98e3cd2127c179
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-07-02 00:43:10 +0530

    binary_number_system.cpp

---
+++ binary_number_system.cpp
@@ -0,0 +1,20 @@
+//Convert from binary to decimal
+#include<iostream>
+using namespace std;
+void binToDec(int binNum){
+    int n=binNum;
+    int decNum = 0;
+    int pow = 1;
+    while(n>0){
+        int lastDig = n%10;
+        decNum +=lastDig*pow;
+        pow = pow*2;
+        n=n/10;
+    }
+    cout<<decNum<<endl;
+}
+
+int main(){
+    binToDec(1011);
+    return 0;
+}
