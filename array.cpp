faa6becd9a5b99bab4f509a117f5499b6d1b8999
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-07-05 02:13:32 +0530

    array.cpp

---
+++ array.cpp
@@ -0,0 +1,12 @@
+//Output & Input Array
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {7,5,2,1,3};
+    int n = sizeof(arr)/sizeof(int);
+    for(int i = 0; i<n; i ++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
