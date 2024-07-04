1a1cf62e8c752b091a3dadefa096728fc06acb96
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-07-05 02:18:36 +0530

    array.cpp

--- array.cpp
+++ array.cpp
@@ -13,3 +13,20 @@ int main(){
 
 //Output : 
 //7 5 2 1 3 
+
+
+//Find largest of array
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={5,4,3,9,12};
+    int n=sizeof(arr)/sizeof(int);
+    int max=arr[0];
+    for(int i=0; i<n; i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    cout<<"largest= "<<max<<endl;
+    return 0;
+}
