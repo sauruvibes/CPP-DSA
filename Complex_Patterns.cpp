832f71bc164b7aef3877f23bab3b8bc95bde4f84
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-18 02:49:12 +0530

    Complex_Patterns.cpp

--- Complex_Patterns.cpp
+++ Complex_Patterns.cpp
@@ -25,3 +25,37 @@ int main(){
     }
     return 0;
 }
+
+
+//Butterfly Pattern
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        for(int j=1; j<=2*(n-i); j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        for(int j=1; j<=2*(n-i); j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
